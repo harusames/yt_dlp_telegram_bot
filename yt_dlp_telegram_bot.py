82a571848165fa14e019e63ae58ba8d0bc27a095
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-06-24 23:09:48 +0300

    Setup basic message handler

--- yt_dlp_telegram_bot.py
+++ yt_dlp_telegram_bot.py
@@ -3,11 +3,17 @@ import logging
 import os
 from datetime import datetime
 
-script_basename = os.path.basename(__file__)
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 
+# region environment variables
 LOGS = os.getenv('LOGS')
 BOT_TOKEN = os.getenv('YT_DLP_TELEGRAM_BOT_BOT_TOKEN')
 WHITELIST_FILE = os.getenv('YT_DLP_TELEGRAM_BOT_WHITELIST_FILE')
+# endregion
+
+
+script_basename = os.path.basename(__file__)
 
 
 def setup_logger():
@@ -37,7 +43,16 @@ def setup_logger():
     return logger
 
 
+async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    trace_id = (str(update.message.date.astimezone()).split('+')[0], update.message.from_user.id)
+    log.info(f'{trace_id} {update.message.from_user.first_name} wrote {update.message.text}')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
 log = setup_logger()
 if __name__ == '__main__':
     log.info(f'Using whitelist file {WHITELIST_FILE}')
-    print()
+
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), message_handler))
+    app.run_polling()
